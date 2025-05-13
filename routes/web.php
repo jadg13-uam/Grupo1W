28871c177ae11e479908056698968f600e15b5d2
megh0302 <megh2003f@gmail.com>
2025-05-13 15:11:18 -0600

    Add new feature: implement migration, controller, route, and view for City

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CityController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+Route::get('/cities', [CityController::class, 'index'])->name('cities.index');
+Route::get('/cities/create', [CityController::class, 'create'])->name('cities.create');
+Route::post('/cities', [CityController::class, 'store'])->name('cities.store');
+Route::get('/cities/{id}', [CityController::class, 'show'])->name('cities.show');
+Route::get('/cities/{id}/edit', [CityController::class, 'edit'])->name('cities.edit');
+Route::put('/cities/{id}', [CityController::class, 'update'])->name('cities.update');
+Route::delete('/cities/{id}', [CityController::class, 'destroy'])->name('cities.destroy');
+
+
+require __DIR__.'/auth.php';
